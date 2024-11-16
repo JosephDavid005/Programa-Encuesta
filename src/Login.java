33aa55a3e4d99bd6a88a2ebffe2bba7a0ee31ff4
bedoy <bedoy@lamaravilla>
2024-11-15 20:28:14 -0500

    ventada de entrada

--- src/Login.java
+++ src/Login.java
@@ -26,34 +26,72 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jPanel2 = new javax.swing.JPanel();
+        lbimg = new javax.swing.JLabel();
+        lbbienvenida = new javax.swing.JLabel();
+        lbnombre = new javax.swing.JLabel();
+        lbcedula = new javax.swing.JLabel();
+        cedula = new javax.swing.JTextField();
+        nombre = new javax.swing.JTextField();
+        btentrar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAutoRequestFocus(false);
+        setBackground(new java.awt.Color(102, 102, 255));
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lbimg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pp.png"))); // NOI18N
+        jPanel2.add(lbimg, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 0, 240, 150));
+
+        lbbienvenida.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Captura de pantalla 2024-11-15 201530.png"))); // NOI18N
+        lbbienvenida.setText("jLabel3");
+        jPanel2.add(lbbienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, -1, -1));
+
+        lbnombre.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Captura de pantalla 2024-11-15 201826.png"))); // NOI18N
+        jPanel2.add(lbnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, -1, -1));
+
+        lbcedula.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Captura de pantalla 2024-11-15 202209.png"))); // NOI18N
+        jPanel2.add(lbcedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, -1, -1));
+
+        cedula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cedulaActionPerformed(evt);
+            }
+        });
+        jPanel2.add(cedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 260, 280, 30));
+        jPanel2.add(nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 190, 280, 30));
+
+        btentrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Captura de pantalla 2024-11-15 202430.png"))); // NOI18N
+        btentrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btentrarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btentrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 340, 130, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(145, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(71, 71, 71)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(133, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 437, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cedulaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cedulaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cedulaActionPerformed
+
+    private void btentrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btentrarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btentrarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -91,7 +129,13 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JButton btentrar;
+    private javax.swing.JTextField cedula;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lbbienvenida;
+    private javax.swing.JLabel lbcedula;
+    private javax.swing.JLabel lbimg;
+    private javax.swing.JLabel lbnombre;
+    private javax.swing.JTextField nombre;
     // End of variables declaration//GEN-END:variables
 }
